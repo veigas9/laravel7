56ea23ef88bc7a142c928dd73c927004b4c7a596
Andre Veigas <l.andreveigas@gmail.com>
2022-10-04 09:59:23 -0300

    Implementado logout das rotas admin

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/', function () {
 */
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
